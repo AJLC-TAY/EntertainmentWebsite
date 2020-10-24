365fd1883a418ec4dae606ee0e0873ba3b9ecba8
ajlcutay <2194014@slu.edu.ph>
2020-10-25 03:22:38 +0800

    changes

--- src/jscript.js
+++ src/jscript.js
@@ -1,25 +1,32 @@
 const APIController = (function () {
     window.onload = loadClientAPI;
-    const API_KEY = 'AIzaSyBmbwKSePliPBSyHyeA6BRuoxgfLES4YeQ';
-    const CLIENT_ID = '471584331255-p66pq86kuef1ocv3jcectp3t9kb1f9bl.apps.googleusercontent.com';
+    // YOUTUBE REQUIREMENTS
+    const YT_API_KEY = 'AIzaSyBmbwKSePliPBSyHyeA6BRuoxgfLES4YeQ';
+    const YT_CLIENT_ID = '471584331255-p66pq86kuef1ocv3jcectp3t9kb1f9bl.apps.googleusercontent.com';
     const BIGHIT_CHANNELID = 'UC3IZKseVpdzPSBaWxBxundA';
+
+    // SPOTIFY REQUIREMENTS
+    const SP_CLIENT_ID = '42aee398a9a943fd839ff19072e88470';
+    const SP_CLIENT_SEC = 'ce15cd96b46444929eec7b641dea0365';
+    const SP_TOKEN = btoa(SP_CLIENT_ID + ":" + SP_CLIENT_SEC);
+
+    // No of elements
     const MAX_RESULTS = '10';
 
-
-    //private methods
+    /** Private methods for Youtube API */
 
     //loads the Youtube API
     function loadClientAPI() {
-        gapi.client.setApiKey(API_KEY);
+        gapi.client.setApiKey(YT_API_KEY);
         gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
             .then(function() { console.log("GAPI client loaded for API"); },
                 function(err) { console.error("Error loading GAPI client for API", err); });
-        gapi.client.init({'clientId': CLIENT_ID});
+        gapi.client.init({'clientId': YT_CLIENT_ID});
     }
 
     // gets the artists data
     const _fetchArtistData = async () => {
-        const result = await fetch('http://localhost/Artists.txt');
+        const result = await fetch('http://bighitmusic.com/Artist.txt');
         console.log(result);
         return await result.json();
     }
@@ -37,6 +44,47 @@ const APIController = (function () {
         return response.result.items;
     }
 
+    /** Private methods for Spotify API */
+
+    // Fetches the token of the Spotify API
+    const _getToken = async () => {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + token
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+
+    // const _getArtist = async (id, token) => {
+    //     const result = await fetch(`https://api.spotify.com/v1/${id}`, {
+    //         method: 'GET',
+    //         headers: { 'Authorization' : 'Bearer ' + token}
+    //     });
+    //
+    //     return (await result.json()).artist;
+    // }
+
+    // Fetches the Top Tracks of an artist by its ID, in a country
+    const _getTopTracks = async (artistId, market, token) => {
+
+        const result = await fetch (`https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=${market}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type' : 'application/json',
+                'Authorization' : 'Bearer ' + token,
+            }
+        });
+
+        return await result.json();
+    }
+
     // use of closures to access the private methods
     return {
 
@@ -47,6 +95,16 @@ const APIController = (function () {
 
         fetchSongs(keyword) {
             return _fetchSongs(keyword);
+        },
+
+        getToken() {
+            return _getToken();
+        },
+        // getArtist(artistId, token) {
+        //     return _getArtist(artistId, token);
+        // },
+        getTopTracks(artistId, market, token) {
+            return _getTopTracks(artistId, market, token);
         }
     }
 
@@ -55,13 +113,20 @@ const APIController = (function () {
 const UIController = (function() {
     const DOMElements = {
         head: 'head',
+        style: '#main-style',
         songList: '#song-list',
         artistList: '#artist-list',
         searchField: '#search-fld',
         searchButton: '#search-btn',
-        filterButtons: '#filters'
+        filterButtons: '#filters',
+
+        // spotify
+        artistList: '#list',
+        topTrackList: '#top-track-list'
+
     }
 
+    /** Private methods */
     const _createSongDetail = async (song) => {
         let title = song.snippet.title;
         let published = song.snippet.publishedAt;
@@ -71,21 +136,24 @@ const UIController = (function() {
             `
                 <div class="video-con">
                     <div class="song-img-con">
-                        <img src="${img.url}" id="imgContainer" alt="Music video" title="${title}" style="width: 100%; height=auto">
+                        <img src="${img.url}" id="imgContainer" 
+                            alt="Music video" title="${title}" 
+                            style="width: 100%; height: auto;">
                     </div>
 
                     <div>
                         <h4>${title}</h4>
                         <p> Artist name: <br>
                         <small>${published}</small><br>
-                        <i>${videoID}</i></p>
+                        </p>
                     </div>
                 </div>
                 `;
+
+
         try {
             document.querySelector(DOMElements.songList).insertAdjacentHTML('beforeend', html);
         } catch (e){}
-
     }
 
     const _createArtistDetail = async (artist) => {
@@ -115,6 +183,7 @@ const UIController = (function() {
 
     const _createArtistList = async (artist) => {
         let name = artist.nickname;
+
         const html = `
             <button class="filter-but" type="button">
                 <p style="margin: 2px 4px;">${name}</p>
@@ -132,6 +201,52 @@ const UIController = (function() {
             document.querySelector(DOMElements.songList).insertAdjacentHTML('beforeend', html);
         } catch (e) {}
     }
+
+    const _createTopTrack = async (track) => {
+        let artistName = track.album.artists[0].name;
+        let trackId = track.id;
+        let artistId = track.album.artists[0].id;
+        let trackName = track.name;
+        let albumName = track.album.name;
+        let releasedate = track.album.release_date;
+        let img = track.album.images[1];
+        // let html = `
+        //     <div class="spotify-song-con" style="display: flex; margin: 10px auto; width: 600px; height: 300px;">
+        //         <img src="${img.url}" alt="Album image" style="width: ${img.width}; height: ${img.height}; margin: auto 20px" >
+        //         <div style="align-items: center; display: block;">
+        //             <h3>${trackName}</h3>
+        //             <p>${artistName} <br>
+        //                 <small>Album name: ${albumName} <br>
+        //                 Released on: ${releasedate}
+        //                 </small>
+        //             </p>
+        //         </div>
+        //     </div>
+        // `;
+
+
+        let html = `
+            <div class="spotify-song-con" style="display: flex; margin: 10px auto; width: 600px; height: 300px;">
+                <img src="${img.url}" alt="Album image" style="width: ${img.width}; height: ${img.height}; margin: auto 20px" >
+                <div style="align-items: center; display: block;">
+                    <h3>${trackName}</h3>
+                    <p>${artistName} <br>
+                        <small>Album name: ${albumName} <br>
+                        Released on: ${releasedate}
+                        </small>
+                    </p>
+                </div>
+            </div>
+        `;
+
+        document.querySelector(DOMElements.topTrackList).insertAdjacentHTML('beforeend', html);
+    }
+
+    const _createArtist = async (artist) => {
+        const html = `<li> "${artist}" </li> `;
+        document.querySelector(DOMElements.artist).insertAdjacentElement('beforeend', html);
+    }
+
 
     return {
 
@@ -163,13 +278,28 @@ const UIController = (function() {
 
         displayNoSongsResults() {
             return _displayNoSongResults()
+        },
+
+        createArtist(artist) {
+            return _createArtist(artist);
+        },
+
+        createTopTrack(track) {
+            return _createTopTrack(track);
         }
+        // ,
+        // getStoredToken() {
+        //     return {
+        //         token: document.querySelector(DOMElements.hfToken).value
+        //     }
+        // }
     }
 })();
 
-
 const APPController = (function (UICtrl, APICtrl) {
     const DOMElements = UICtrl.inputOutputFields();
+    const artistsDetail = Array();
+    const SP_NL_RESULT = 3;
 
     const loadArtists = async () => {
         const artists = await APICtrl.fetchArtistData();
@@ -180,7 +310,20 @@ const APPController = (function (UICtrl, APICtrl) {
             UICtrl.createArtistList(artist);
             // artist page
             UICtrl.createArtistDetail(artist);
+
+            let artistDet = Object();
+            artistDet.name = artist.name;
+            artistDet.nickName = artist.nickname;
+            artistDet.yId = artist.yt;
+            artistDet.sId = artist.sp;
+
+            artistsDetail.push(artistDet);
         });
+
+        console.log(artistsDetail);
+    }
+
+    const filterClicked = async () => {
 
     }
 
@@ -207,6 +350,5 @@ const APPController = (function (UICtrl, APICtrl) {
         }
     }
 })(UIController, APIController);
-
 
 APPController.init();
