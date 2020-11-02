963ee520d3d34985683124aac8d33dadb38262eb
Gwyneth Calica <2190485@slu.edu.ph>
2020-11-03 05:11:11 +0800

    Creation of Hamburger icon

--- src/jscript.js
+++ src/jscript.js
@@ -597,9 +597,26 @@ const APPController = (function (UICtrl, APICtrl) {
 
 APPController.init();
 
-// Slideshow animation
-// Gwyneth
 
+// Gwyneth
+// Hamburger icon
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-link');
+  const navLinks = document.querySelectorAll('.nav-link li');
+  //Toggle Nav
+  burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+  });
+  //Animate Links
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
+
+  })
+}
+
+navSlide();
+// Slideshow animation
 var slideIndex = 0;
 try {
     showSlides();
