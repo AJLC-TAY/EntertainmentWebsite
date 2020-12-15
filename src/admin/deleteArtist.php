151c5780ec336c1712ef9ddc0c1e15ebddde25b9
Gwyneth Calica <2190485@slu.edu.ph>
2020-12-15 13:28:33 +0800

    Added comments in the deleteArtist.php

--- src/admin/deleteArtist.php
+++ src/admin/deleteArtist.php
@@ -1,8 +1,13 @@
+<!--
+This deletes an artist record in the artist admin page
+@author: Calica, Gwyneth
+-->
 <?php
     include ("../includes/sessionHandling.php");
     include "../includes/database.php";
     $artistid = $_GET['id'];
 
+    //Select the artist resord to be deleted
     $query = "SELECT artistimage FROM artists WHERE artistid='$artistid'";
     $dbase = $database->stmt_init();
     $dbase ->prepare($query);
@@ -11,6 +16,7 @@
     $dbase ->fetch();
     $dbase ->close();
 
+    //Deletes the artist record from the database
     $query = "DELETE FROM artists WHERE artistid='$artistid'";
     $dbase = $database->stmt_init();
     $dbase ->prepare($query);
