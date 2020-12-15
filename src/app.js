fbadf84cc070d4b72d8f7b5bae64e6fd89d68f60
2192011 <2192011@slu.edu.ph>
2020-12-15 15:30:21 +0800

    1. Added comments to app.js 2. Edited some lines in playlist.pug and artists.pug

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* Node JS modules used in our website */
 const express = require('express');
 const mysql = require('mysql');
 const btoa = require('btoa');
@@ -9,7 +10,7 @@ app.set('views', `${__dirname}/view`);
 app.set('view engine', 'pug');
 
 const connection = mysql.createConnection({
-    host: 'localhost', user: 'root', password: '', database: 'bighitent', port:'3306'
+    host: 'localhost', user: 'root', password: '', database: 'bighitent', port:'3308'
 });
 connection.connect((err) => {
     if(err) {
@@ -19,16 +20,33 @@ connection.connect((err) => {
     }
 });
 
+/* Our website listens on port 8001 with the bighitmusic.com domain */
 app.listen(8001, 'bighitmusic.com');
 
+/**
+ * Redirects the user to the index page if the user entered bighitmusic.com:8001
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/', (request, response) => {
     response.render('index');
 });
 
+/**
+ * Redirects the user to the index page if the user entered bighitmusic.com:8001/index,
+ * when the user clicks home on the navigation bar, or when the user clicks the logo
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/index', (request, response) => {
     response.render('index');
 });
 
+/**
+ * Redirects the user to the songs page if the user entered bighitmusic.com:8001/songs or
+ * when the user clicks music on the navigation bar.
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/songs', (request, response) => {
     const query1 = `SELECT albumid, albumimg, albumname, artists.artistname, releaseddate, tracks.name AS trackname,
             tracks.trackid, tracks.musicfile, tracks.musicvideo FROM albums JOIN artists USING(artistid) JOIN tracks USING(albumid)`;
@@ -44,6 +62,11 @@ app.get('/songs', (request, response) => {
     });
 });
 
+/**
+ * Fetches the tracks from our database to be parsed on the songs page
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 function getTracks(query) {
     return  new Promise(function (resolve, reject) {
 
@@ -97,6 +120,11 @@ function getAlbums(albumid) {
     });
 }
 
+/**
+ * Redirects the user to the videos page if the user click see vid on the songs page under a track
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/videos', (request, response) => {
     var trackid = request.query.seeVid;
     getVid(trackid).then(function (tracks) {
@@ -104,6 +132,11 @@ app.get('/videos', (request, response) => {
     });
 });
 
+/**
+ * Fetches the youtube video key from our database which is embedded to an iframe to play the video
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 function getVid(trackid) {
     return  new Promise(function (resolve, reject) {
         const query = `SELECT name, musicvideo FROM tracks WHERE trackid = ?`;
@@ -119,6 +152,11 @@ function getVid(trackid) {
     });
 }
 
+/**
+ * Filters the tracks shown under the songs page when the user clicks a filter button
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/filter', (request, response) => {
     var artistid = request.query.filterArtist;
     var query1 = `SELECT albumid, albumimg, albumname, artists.artistname, releaseddate, tracks.name AS trackname,
@@ -134,6 +172,11 @@ app.get('/filter', (request, response) => {
     });
 });
 
+/**
+ * Fetches the image of an album from the database to be used on the playlist
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 function getImage(albumid) {
     return  new Promise(function (resolve, reject) {
         const query = `SELECT albumimg, albumname FROM albums WHERE albumid = ?`;
@@ -150,6 +193,11 @@ function getImage(albumid) {
     });
 }
 
+/**
+ * Filters the tracks shown under the songs page based on the keyword when the user uses the search functionality
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/search', (request, response) => {
     var keyword = request.query.searchTracks;
     const query1 = `SELECT albumid, albumimg, albumname, artists.artistname, releaseddate, tracks.name AS trackname,
@@ -168,21 +216,14 @@ app.get('/search', (request, response) => {
 });
 
 app.get('/artists', (request, response) => {
-    const query = `SELECT artistid, artistname, nickname, artistimage, debutyear, membernum FROM artists WHERE artistid != 61`;
+    var query = `SELECT artistid, artistname, nickname, artistimage, debutyear, membernum FROM artists WHERE artistid != 61`;
     getArtists(query).then(function(artists) {
-        artists.forEach(artist => {
-            if(artist.artistimage == null) {
-                console.log("No Image");
-            } else {
-                artist.artistimage = "data:image;base64," + btoa(artist.artistimage);
-            }
-        });
         response.render('artists', {artists: artists});
     });
 });
 
 function getArtists(query) {
-    return  new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
         connection.query(query, (err, result) => {
             if (err) {
                 console.log('Unsuccessful');
@@ -195,6 +236,12 @@ function getArtists(query) {
     });
 }
 
+/**
+ * Redirects the user to the about page if the user entered bighitmusic.com:8001/about or
+ * when the user clicks about on the navigation bar.
+ *
+ * @author Kilrone Yance B. Del-ong
+ */
 app.get('/about', (request, response) => {
     response.render('about');
 });
